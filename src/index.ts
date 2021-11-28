a7a6663df119c9802ed5ecec8b6a0fdf68e98ad7
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-28 22:58:46 +0100

    export logic from index

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,9 @@
 require('dotenv').config();
-const { Client } = require("@notionhq/client");
-const dayjs = require('dayjs');
+import { Client } from "@notionhq/client";
+import { jobs } from "./programmed_jobs";
 
-// Initializing a client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
-
-(async () => {
-  const data = await notion.databases.query({
-    database_id: process.env.DATABASE_ID,
-    filter: {
-      property: "stage",
-      select: {
-        equals: "wrong",
-      },
-    },
-  });
-
-  Promise.all(data.results.map(async (obj: any) => {
-    await notion.pages.update({
-      page_id: obj.id,
-      properties: {
-        stage: {
-          type: "select",
-          select: {
-            name: "0"
-          }
-        },
-        'wrong date': {
-          date: {
-            start: dayjs().format('YYYY-MM-DD')
-          }
-        }
-      }
-    });
-  }));
-
-})();
+console.log(new Date());
+jobs(notion);
