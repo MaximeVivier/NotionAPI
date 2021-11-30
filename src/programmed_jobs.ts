f4dce8245f23919f20a6bccafdc1a036f7069caf
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-30 23:19:34 +0100

    update jobs timing to moitor

--- src/programmed_jobs.ts
+++ src/programmed_jobs.ts
@@ -3,7 +3,7 @@ import wrongVocabFrenchEnglish from './actions/updates/flashcard/wrongToStage0';
 import type { Client } from "@notionhq/client";
 
 export const jobs = (notion: Client) => {
-  schedule.scheduleJob('45 23 * * *', async () => {
+  schedule.scheduleJob('28 22 * * *', async () => {
     console.log('Job executed in notion');
     console.log(new Date());
     await wrongVocabFrenchEnglish(notion);
